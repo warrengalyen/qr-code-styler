ca2f0d0b94d2c4234ec999b4c2012de366e9b0e1
wgalyen <wgalyen@bellhelmets.com>
2019-11-06 14:05:25 -0800

    Added styling API

--- app/index.js
+++ app/index.js
@@ -1,11 +1,27 @@
 import QrCodeStyler from '../src';
-import logo from "./assets/fb_logo_resized.png";
+import logo from "./assets/fb_logo.png";
 
 const qrCode = new QrCodeStyler({
     width: 300,
     height: 300,
-    data: "Hello!",
-    image: logo
+    data: "https://www.bellhelmets.com",
+    image: logo,
+
+    qrOptions: {
+        // typeNumber: 10,
+        // errorCorrectionLevel: "H",
+    },
+    imageOptions: {
+        hideBackgroundDots: true,
+        imageSize: 0.4
+    },
+    dotsOptions: {
+        color: "#4267b2",
+        styles: "dots"
+    },
+    backgroundOptions: {
+        color: "#e9ebee"
+    }
 });
 
 qrCode.append("#canvas");
